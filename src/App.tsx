7f816c085dcc47af7996f207a5be54ffbf210815
Jasurbek Shomaqsudov <107366287+Jasurbek2208@users.noreply.github.com>
2024-04-04 00:37:35 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { GlobalStyle } from 'assets/styles/global'
 import Hangman from 'pages/Hangman'
 
 export default function App() {
   return (
-    <Router>
-      <>
-        <GlobalStyle />
-        <Switch>
-          <Route exact path="/" component={Hangman} />
-        </Switch>
-      </>
-    </Router>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Hangman />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
