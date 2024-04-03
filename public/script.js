781fadfd73dec63b4ee63d8b31f31008de2e4eb6
Jasurbek Shomaqsudov <107366287+Jasurbek2208@users.noreply.github.com>
2024-04-03 22:02:06 +0500

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,12 @@
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch((error) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
