cf3e6a7068496f945e8440157d24b2f1e9ba00cf
Jasur <jasurbekshomaqsudov2004@gmail.com>
2024-04-04 10:18:34 +0300

    refactor: added types for /src/assets/styles/global.js

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -1,18 +1,18 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, GlobalStyleComponent, DefaultTheme } from 'styled-components'
 import './fonts.css'
 
-export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+export const GlobalStyle: GlobalStyleComponent<{}, DefaultTheme> = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    .container {
-        padding: 0px 16px;
-        margin: 0 auto;
-        max-width: 700px;
-        height: 100vh;
-        font-family: 'Inter', sans-serif;
-    }
+  .container {
+    padding: 0px 16px;
+    margin: 0 auto;
+    max-width: 700px;
+    height: 100vh;
+    font-family: 'Inter', sans-serif;
+  }
 `
